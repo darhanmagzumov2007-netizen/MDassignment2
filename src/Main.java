bef504e6e7aff2221b92614407081f72a123e13d
darhanmagzumov2007-netizen <darhanmagzumov2007@gmail.com>
2026-01-02 16:11:08 +0500

    darkhan

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
-public class Main {
+import models.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main{
+    public static void main(String[] args) {
+        ArrayList<Person> list = new ArrayList<>(); // [cite: 35]
+
+        list.add(new Employee("Aibar", "Erkebulan", "Lawyer", 26512.78));
+        list.add(new Employee("Sultanbek", "Aibekuly", "Builder", 52342.00));
+        list.add(new Student(" Batyrkhan", "Manarbekuly", 2.16));
+        list.add(new Student("Erasyl", "Abdarakhamanov", 4.0));
+
+
+        Collections.sort(list);
+
+        printData(list);
+    }
+
+    public static void printData(Iterable<Person> people) {
+        for (Person p: people) {
+
+            System.out.println(p.toString() + " earns " + String.format("%s", p.getPaymentAmount()) + " tenge");
+        }
+    }
 }
